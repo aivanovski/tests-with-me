67f9358a7fea8b3ac3bbc792dc4c86da99e01c40
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2024-07-27 10:29:22 +0200

    Add .editorconfig file

--- android-app/src/main/java/com/github/aivanovski/testwithme/android/presentation/screens/flow/cells/model/FlowHistoryCellModel.kt
+++
@@ -1,7 +0,0 @@
-package com.github.aivanovski.testwithme.android.presentation.screens.flow.cells.model
-
-import com.github.aivanovski.testwithme.android.presentation.core.cells.BaseCellModel
-
-data class FlowHistoryCellModel(
-    override val id: String,
-) : BaseCellModel(id)
