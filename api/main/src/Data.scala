e86da4b960014605497750aacbe536033db0d7c3
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2025-03-06 21:01:22 +0100

    Remove unnecessary data from api util

--- api/main/src/Data.scala
+++ api/main/src/Data.scala
@@ -19,57 +19,16 @@ object Data {
           "repositoryUrl" -> "https://github.com/aivanovski/keepassvault-tests.git"
         )
       ),
-//      () => api.postProject(
-//        Map(
-//          "name" -> "F-Droid",
-//          "packageName" -> "org.fdroid.fdroid",
-//          "description" -> "Market of FOSS applications",
-//          "imageUrl" -> "https://gitlab.com/uploads/-/system/project/avatar/36189/ic_launcher.png",
-//          "downloadUrl" -> "https://f-droid.org",
-//          "siteUrl" -> "https://f-droid.org",
-//        )
-//      ),
-//
-//      // Groups
-//      () => api.postGroup(path = "KeePassVault/Root", name = "Screens"),
-//      () => api.postGroup(path = "KeePassVault/Root", name = "Common"),
-//      () => api.postGroup(path = "KeePassVault/Root/Screens", name = "Group"),
-//      () => api.postGroup(path = "KeePassVault/Root/Screens", name = "Unlock"),
-//      () => api.postGroup(path = "KeePassVault/Root/Screens", name = "New Database"),
-//
-//      // Flows
-//      () => api.postFlow(
-//        path = "KeePassVault/Root/Common",
-//        content = readFile(s"$FLOW_DIRECTORY_PATH/reset-data-and-setup-basic.kdbx-database.yaml")
-//      ),
-//      () => api.postFlow(
-//        path = "KeePassVault/Root/Common",
-//        content = readFile(s"$FLOW_DIRECTORY_PATH/reset-data.yaml")
-//      ),
-//      () => api.postFlow(
-//        path = "KeePassVault/Root/Screens/New Database",
-//        content = readFile(s"$FLOW_DIRECTORY_PATH/create-new-database-file-with-templates.yaml")
-//      ),
-//      () => api.postFlow(
-//        path = "KeePassVault/Root/Screens/New Database",
-//        content = readFile(s"$FLOW_DIRECTORY_PATH/create-new-database-file.yaml")
-//      ),
-//      () => api.postFlow(
-//        path = "KeePassVault/Root/Screens/Group",
-//        content = readFile(s"$FLOW_DIRECTORY_PATH/create-new-group.yaml")
-//      ),
-//      () => api.postFlow(
-//        path = "KeePassVault/Root/Screens/Unlock",
-//        content = readFile(s"$FLOW_DIRECTORY_PATH/unlock-database-with-key-file-and-password.yaml")
-//      ),
-//      () => api.postFlow(
-//        path = "KeePassVault/Root/Screens/Unlock",
-//        content = readFile(s"$FLOW_DIRECTORY_PATH/unlock-database-with-key-file.yaml")
-//      ),
-//      () => api.postFlow(
-//        path = "KeePassVault/Root/Screens/Unlock",
-//        content = readFile(s"$FLOW_DIRECTORY_PATH/unlock-database-with-password.yaml")
-//      ),
+      () => api.postProject(
+        Map(
+          "name" -> "F-Droid",
+          "packageName" -> "org.fdroid.fdroid",
+          "description" -> "Market of FOSS applications",
+          "imageUrl" -> "https://gitlab.com/uploads/-/system/project/avatar/36189/ic_launcher.png",
+          "downloadUrl" -> "https://f-droid.org",
+          "siteUrl" -> "https://f-droid.org",
+        )
+      ),
     )
 
     for (response <- requests) printResponse(response.apply())
