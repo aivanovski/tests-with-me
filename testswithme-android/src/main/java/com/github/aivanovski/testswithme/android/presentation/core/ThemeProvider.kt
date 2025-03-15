703b8b005437e6cf3d6b95eaae2b8c9d0b3012c3
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2025-03-15 10:17:15 +0100

    [ANDROID] Removed old ThemeProvider

--- testswithme-android/src/main/java/com/github/aivanovski/testswithme/android/presentation/core/ThemeProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.github.aivanovski.testswithme.android.presentation.core
-
-import com.github.aivanovski.testswithme.android.presentation.core.compose.theme.Theme
-
-@Deprecated("Should be removed")
-interface ThemeProvider {
-    fun getCurrentTheme(): Theme
-}
